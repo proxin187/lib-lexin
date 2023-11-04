2f464d273fd87500ffc104256df7707e53e93124
proxin187 <thugshaker627@gmail.com>
2023-11-04 09:38:41 +0100

    i forgor :(

--- src/lib.rs
+++ src/lib.rs
@@ -274,7 +274,6 @@ impl Lexer {
                             token = token + &(self.buffer[index] as char).to_string();
                         }
                     } else if self.is_section(Value::End(section[0].start.to_string(), character.clone())).is_ok() || index + 2 >= self.buffer.len() { // index doesnt matter here because all indexes has the same start
-                        println!("Closed");
                         token = token + &character;
                         self.lex_token(&token, loc).map(|t| tokens.push(t));
                         section = Vec::new();
